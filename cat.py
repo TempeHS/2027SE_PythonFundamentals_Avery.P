6c4c9ee893a1afa67706d2c7f01238a1760fc896
Avery P <159206600+AveryPilcer@users.noreply.github.com>
2026-02-19 03:15:20 +0000

    Video 34:49

---
+++ cat.py
@@ -0,0 +1,34 @@
+def main():
+    number = get_number()
+    meow(number)
+
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+    return n
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+main()
+
+
+# while True:
+#    n = int(input("what's n? "))
+#    if n > 0:
+#        break
+# for _ in range(n):
+#    print("meow")
+# for _ in range(3):
+#    print("meow")
+# count = 0
+# while count != 3:
+#    print("meow")
+#    count += 1
+# print("meow\n" * 3, end="")
