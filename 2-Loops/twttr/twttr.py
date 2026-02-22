8e211ce019006b588c55b4a35690b39f5ff8ebb5
Avery P <159206600+AveryPilcer@users.noreply.github.com>
2026-02-22 04:14:56 +0000

    Struggling on problem 2.3

--- 2-Loops/twttr/twttr.py
+++ 2-Loops/twttr/twttr.py
@@ -1,8 +1,2 @@
 twtter = input("What's your message? ").casefold
-twtter = twtter.replace("a", "")
-twtter = twtter.replace("e", "")
-twtter = twtter.replace("i", "")
-twtter = twtter.replace("o", "")
-twtter = twtter.replace("u", "")
-twtter = twtter.capitalize()
-print(twtter)
+
