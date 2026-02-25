05a8c3d824111029e79d300136014db922d44f9a
Avery P <159206600+AveryPilcer@users.noreply.github.com>
2026-02-25 07:00:52 +0000

    Problem 2.4 Completed

--- 2-Loops/plates/plates.py
+++ 2-Loops/plates/plates.py
@@ -7,7 +7,44 @@ def main():
 
 
 def is_valid(s):
-    if s[0:2].isalpha():
+    if length(s) and beginnum(s) and ischar(s) and middlenum(s):
+        return True
+
+
+def length(l):
+    if len(l) > 6 or len(l) < 2:
+        return False
+    else:
+        return True
+
+
+def beginnum(n):
+    if n[0:2].isalpha():
+        return True
+    else:
+        return False
+
+
+def ischar(c):
+    if c.isalnum():
+        return True
+    else:
+        return False
+
+
+def middlenum(m):
+    lrn = len(m) - 2
+    lon = len(m) - 1
+    ltn = len(m) - 3
+    lan = len(m) + 1
+    if (
+        m[2:ltn].isnumeric()
+        or m[3:lrn].isnumeric()
+        or m[4:lon].isnumeric()
+        and m[len(m) : lan].isalpha()
+    ):
+        return False
+    else:
         return True
 
 
