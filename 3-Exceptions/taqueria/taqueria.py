e0672c86914565b9b69eb791139b19697d197ac1
Avery P <159206600+AveryPilcer@users.noreply.github.com>
2026-02-27 01:50:11 +0000

    Started 3.2

---
+++ 3-Exceptions/taqueria/taqueria.py
@@ -0,0 +1,16 @@
+menus = [
+    {
+        "Baja Taco": 4.25,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00,
+    }
+]
+x = 0
+
+print(x)
