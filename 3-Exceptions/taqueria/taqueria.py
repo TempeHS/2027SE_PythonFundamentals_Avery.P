ab4e14162c6c73a5329860c554bcfb5ad1e00a23
Avery P <159206600+AveryPilcer@users.noreply.github.com>
2026-02-28 01:54:01 +0000

    3.2 completed

--- 3-Exceptions/taqueria/taqueria.py
+++ 3-Exceptions/taqueria/taqueria.py
@@ -1,16 +1,36 @@
 menus = [
     {
-        "Baja Taco": 4.25,
-        "Burrito": 7.50,
-        "Bowl": 8.50,
-        "Nachos": 11.00,
-        "Quesadilla": 8.50,
-        "Super Burrito": 8.50,
-        "Super Quesadilla": 9.50,
-        "Taco": 3.00,
-        "Tortilla Salad": 8.00,
+        "baja taco": 4.25,
+        "burrito": 7.50,
+        "bowl": 8.50,
+        "nachos": 11.00,
+        "quesadilla": 8.50,
+        "super burrito": 8.50,
+        "super quesadilla": 9.50,
+        "taco": 3.00,
+        "tortilla salad": 8.00,
     }
 ]
 x = 0
+while True:
+    ask = input("What would you like to order? ").casefold()
+    if ask != "End" or ask != "end":
+        pass
+    else:
+        break
+    for menu in menus:
+        if ask in menu:
+            x += menu[ask]
+            break
+        else:
+            pass
+    question = input("Would you like to order more food? ")
+    if question == "Yes" or question == "yes" or question == "y" or question == "Y":
+        pass
+    elif question == "No" or question == "no" or question == "n" or question == "N":
+        break
+    else:
+        print("Unclear answer looping")
+        pass
 
-print(x)
+print("Total:", x)
