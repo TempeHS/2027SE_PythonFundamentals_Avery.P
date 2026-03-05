4824754040a63f7d20fe13814c95c7d39be48a56
Avery P <159206600+AveryPilcer@users.noreply.github.com>
2026-03-05 02:50:53 +0000

    4.2 completed

--- 4-Libraries/figlet/figlet.py
+++ 4-Libraries/figlet/figlet.py
@@ -2,11 +2,22 @@ from pyfiglet import Figlet
 import sys
 
 s = input("What is your message? ")
+
+
 figlet = Figlet()
 figlet.getFonts()
-figlet.setFont(font=f)
 
-if len(sys.argv) < 2:
-    sys.exit("Too few arguments")
+
+if len(sys.argv) == 1:
+    pass
+
+elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"]:
+    f = sys.argv[2]
+    if f not in figlet.getFonts():
+        sys.exit("Wrong Font")
+    figlet.setFont(font=f)
+
+else:
+    sys.exit("You messed up something")
 
 print(figlet.renderText(s))
