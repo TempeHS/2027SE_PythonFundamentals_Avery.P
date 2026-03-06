ae45f82875410b7f86d9467215e925c1a9bb3c38
Avery P <159206600+AveryPilcer@users.noreply.github.com>
2026-03-06 00:55:52 +0000

    Started 4.5

---
+++ 4-Libraries/professor/professor.py
@@ -0,0 +1,18 @@
+import random
+def main():
+  	
+
+
+def get_level():
+    while True:
+	    try:
+            lvl = int(input("Level"))
+	
+
+
+def generate_integer(level):
+
+
+
+if __name__ == "__main__":
+main()
