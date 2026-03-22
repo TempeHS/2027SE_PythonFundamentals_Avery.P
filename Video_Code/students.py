6714610fee6055dbc55d018d233c89e94e3ab6f5
Avery P <159206600+AveryPilcer@users.noreply.github.com>
2026-03-22 23:19:45 +0000

    Video Completed

--- Video_Code/students.py
+++ Video_Code/students.py
@@ -1,14 +1,23 @@
-students = []
+import csv
 
-with open("names.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+name = input("What's  your name? ")
+home = input("What's your home? ")
 
+with open("names.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
 
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")
+# import csv
+
+# students = []
+
+# with open("names.csv") as file:
+#    reader = csv.DictReader(file)
+#    for row in reader:
+#        students.append({"name": row["name"], "house": row["home"]})
+
+# for student in sorted(students, key=lambda student: student["name"]):
+#    print(f"{student['name']} is in {student['house']}")
 
 
 # with open("names.csv") as file:
